a7d678172b19d2eb3967b160819c44edd29eb2be
Wilfred Hughes <wilfred@meta.com>
2026-04-27 08:07:10 -0700

    Remove swift-format-ignore-file comment from test files (4/4)
    Summary:
    Remove the `swift-format-ignore-file: OrderedImports` comment from test files that were part of D102170960, and run the formatter to fix import ordering.
    
    swift-format 6.3 landed in D102351570. I've limited the cleanup to test files because there are a few cases where reordering imports can change the meaning of code: https://github.com/swiftlang/swift/issues/76842, whereas test files are safe to codemod (sandcastle is sufficient).
    
    allow-large-files
    allow_many_files
    bypass-lint
    drop-conflicts
    
    Reviewed By: ebgraham
    
    Differential Revision: D102595847
    
    fbshipit-source-id: be04ed27579d02e39185e9f861c774aba4ffa5f0

--- Sources/QuickLayout/QuickLayoutBridge/__server_snapshot_tests__/HStackRTLServerSnapshotTests.swift
+++ Sources/QuickLayout/QuickLayoutBridge/__server_snapshot_tests__/HStackRTLServerSnapshotTests.swift
@@ -5,12 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// Disabled during swift-format 6.3 rollout, feel free to remove:
-// swift-format-ignore-file: OrderedImports
-
 import FBServerSnapshotTestCase
 import FBTestImageGenerator
-
 @testable import QuickLayoutBridge
 @testable import QuickLayoutCore
 
